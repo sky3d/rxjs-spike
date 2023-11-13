92306624e2bb0951a7654583d7ee5a97d41a5e1e
sky3d <sky3ddd@gmail.com>
2023-11-13 10:49:50 +0300

    feat: node-abort-controller

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,56 @@
+import { AbortController, AbortSignal } from 'node-abort-controller'
 
-const delay = (time: number) => new Promise(resolve => setTimeout(resolve, time))
+const kAbortError = new Error('Abort')
 
-async function main() {
+const TASK_TIME = 3000
+const ABORT_TIME = 1000
 
-  console.log('Start')
-  await delay(1000)
+const print = (s: string, ...args: any[]) => console.log(`${Math.round(Date.now() / 1000)}: ${s}`, ...args)
+
+function cancelTask(controller: AbortController, time: number) {
+  setTimeout(() => {
+    print('===> throw abort')
+    controller.abort()
+  }, time)
 }
 
-main()
-  .then(() => {
-    console.log('Done!')
+function runTask(time: number, signal: AbortSignal): Promise<void | Error> {
+  return new Promise((resolve, reject) => {
+    print('processing')
+
+    signal.addEventListener('abort', () => {
+      print('on abort:', signal.aborted)
+      clearTimeout(timeoutId)
+      reject(kAbortError)
+    })
+
+    const timeoutId = setTimeout(resolve, time)
+
+    if (signal.aborted) {
+      clearTimeout(timeoutId)
+    }
   })
-  .catch((e) => console.error(e))
+}
+
+async function main() {
+  const controller = new AbortController()
+  const { signal } = controller
+
+  print('running')
+
+  print('start-->')
+  runTask(TASK_TIME, signal)
+    .then(() => {
+      print('<--end')
+    })
+    .catch((err) => {
+      print('catch error:' + err?.message)
+    })
+
+  // simulate interrupt
+  cancelTask(controller, ABORT_TIME)
+
+  print('done')
+}
+
+main().catch((e) => console.error(e))
