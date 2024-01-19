78a941d58d7b32a7e5ff0fe99b34df831de86889
sky3d <sky3ddd@gmail.com>
2024-01-19 17:24:28 +0300

    feat: more rxjs samples

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,5 +1,7 @@
-import { BehaviorSubject, Observable, Subject, from, range } from 'rxjs'
-import { filter, map, tap, takeUntil, takeWhile, concatMap } from 'rxjs/operators'
+import { BehaviorSubject, Observable, Subject, from, interval, range } from 'rxjs'
+import { 
+  filter, map, tap, takeUntil, takeWhile,  concatMap, finalize, ignoreElements, take 
+} from 'rxjs/operators'
 import { times } from 'lodash'
 import { faker } from '@faker-js/faker'
 
@@ -29,6 +31,24 @@ const initUsers = (n: number) => times(n, (index) => ({
   role: random(usersRoles)
 }))
 
+const updateUser = async(user: User): Promise<User> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      user.updatedAt = Date.now()
+      resolve(user)
+    }, 500)
+  })
+}
+
+const performTask = async(ts: number): Promise<number> => {
+  return new Promise((resolve, _) => {
+    setTimeout(() => {
+      console.log(`.....task executed after ${ts} ms`)
+      resolve(Date.now())
+    }, ts)
+  })
+}
+
 
 function cancelOn(observable: Observable<User>, id: number) {
   const cancelWithLimit = new Subject<void>();
@@ -46,9 +66,31 @@ function cancelOn(observable: Observable<User>, id: number) {
   )
 }
 
+function withIgnoreElements() {
+    //emit value every 100ms
+  const source = interval(100);
+  //ignore everything but complete
+  const example = source
+    .pipe(
+      take(5), 
+      tap((x) => console.log(x)),
+      // Do not call next(N) but 
+      ignoreElements() 
+      // Call complete
+  );
+  //output: "COMPLETE!"
 
-function filterUsers(observable: Observable<User>, types: string[], ) {
-  
+/* 
+  * Params: next, error, complete
+  */
+  const subscribe = example.subscribe(
+    val => console.log(`NEXT: ${val}`),
+    val => console.log(`ERROR: ${val}`),
+    () => console.log('COMPLETE!')
+  );
+}
+
+function filterUsers(observable: Observable<User>, types: string[]) {
   return observable
   .pipe(
     filter((x) => x.role === 'user'),
@@ -66,13 +108,19 @@ function takeTopN(observable: Observable<User>, num: number) {
   )
 }
 
-const updateUser = async(user: User): Promise<User> => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      user.updatedAt = Date.now()
-      resolve(user)
-    }, 500)
-  })
+function finalizeSample(observable: Observable<User>, types: string[], ) {
+  return observable
+  .pipe(
+    filter((x) => x.role === 'user'),
+    concatMap(updateUser),
+    tap((x) => console.log(`user ${JSON.stringify(x)} updated!`)),
+    ignoreElements(), // suppress NEXT calls
+    finalize(async () => { 
+        console.log(`executing...`, Date.now())
+        const time = await performTask(2000); 
+        console.log(`finalize ok!`, time)
+     })
+  )
 }
 
 export const runPipe = () => {
@@ -83,6 +131,8 @@ export const runPipe = () => {
 
   let res
 
+  //withIgnoreElements()
+
   let observable = from(users)
   
   // observable = takeTopN(observable, 7)
@@ -91,12 +141,21 @@ export const runPipe = () => {
   // observable = cancelOn(observable, 7)
   // console.log('[x] canceling on 5')
 
-  observable = filterUsers(observable, ['user'])
-  console.log('[x] filtering')
+  // observable = filterUsers(observable, ['user'])
+  // console.log('[x] filtering')
+
+  observable = finalizeSample(observable, ['user'])
+  console.log('pipe with finalize called')
   
   res = observable.subscribe({
     complete: () => {
-      console.log('complete')
+      console.log('[x] COMPLETE')
+    },
+    next: (x) => {
+      console.log(`NEXT --> ${JSON.stringify(x)}`)
+    },
+    error: (err) => {
+      console.log(`! ERROR: ${err}`)
     }
   })
 }  
